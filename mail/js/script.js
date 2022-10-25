679ac99f6702c79839cf388ff2cee840892b8aff
DavideSias <davidesias77@gmail.com>
2022-10-25 17:05:30 +0200

    Add html

--- mail/js/script.js
+++ mail/js/script.js
@@ -13,13 +13,22 @@ Stampare un messaggio appropriato sull’esito del controllo.
 
 const arrMail = ['prova0@mail.com','prova1@mail.com', 'prova2@mail.com', 'prova3@mail.com', 'prova4@mail.com', 'prova5@mail.com', 'prova6@mail.com', 'prova7@mail.com', 'prova8@mail.com', 'prova9@mail.com', 'prova10@mail.com'];
 
-const eleMail = prompt('inserisci la tua mail');
+const check = document.getElementById('check');
 
-if (arrMail.includes(eleMail)){
-    console.log('La mail è corretta puoi entrare')
-} else {
-    console.log('La mail è errata')
+check.addEventListener('click', function(){
+    
+    const eleMail = document.querySelector('.mail').value;
+    const yourMail = document.getElementById('your-mail');
+
+    if (arrMail.includes(eleMail)){
+        yourMail.innerHTML = `Ciao ${eleMail} la mail è corretta puoi entrare`;
+    } else {
+        yourMail.innerHTML = 'La mail non è corretta, riprovare';
+    }
 }
+)
+
+
 
 
 
