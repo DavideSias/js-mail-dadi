fee16761b53739813bca710f8caa38da1f11b6b4
DavideSias <davidesias77@gmail.com>
2022-10-26 10:56:25 +0200

    Refactor js script

--- mail/js/script.js
+++ mail/js/script.js
@@ -11,22 +11,51 @@ Controlla che la mail inserita sia nella lista di chi può accedere;
 Stampare un messaggio appropriato sull’esito del controllo.
 */
 
-const arrMail = ['prova0@mail.com','prova1@mail.com', 'prova2@mail.com', 'prova3@mail.com', 'prova4@mail.com', 'prova5@mail.com', 'prova6@mail.com', 'prova7@mail.com', 'prova8@mail.com', 'prova9@mail.com', 'prova10@mail.com'];
+const arrMail = [
+    'prova0@mail.com',
+    'prova1@mail.com',
+    'prova2@mail.com',
+    'prova3@mail.com',
+    'prova4@mail.com',
+    'prova5@mail.com',
+    'prova6@mail.com',
+    'prova7@mail.com',
+    'prova8@mail.com',
+    'prova9@mail.com',
+    'prova10@mail.com',
+];
 
 const check = document.getElementById('check');
 
 check.addEventListener('click', function(){
     
-    const eleMail = document.querySelector('.mail').value;
+    const eleMail = document.querySelector('.mail');
     const yourMail = document.getElementById('your-mail');
 
-    if (arrMail.includes(eleMail)){
+    const mailSearched = eleMail.value;
+    let emailFound = false;
+
+    for(let i = 0; i < arrMail.length; i++) {
+        if(mailSearched === arrMail[i]) {
+            emailFound = true;
+        } 
+    }
+
+    if(emailFound) {
         yourMail.innerHTML = `Ciao ${eleMail} la mail è corretta puoi entrare.`;
     } else {
         yourMail.innerHTML = `Ciao ${eleMail} la mail non è presente nel nostro sistema, riprovare.`;
     }
+
+    // ALTERNATIVA CON INCLUDES
+    // if (arrMail.includes(eleMail)){
+    //     yourMail.innerHTML = `Ciao ${eleMail} la mail è corretta puoi entrare.`;
+    // } else {
+    //     yourMail.innerHTML = `Ciao ${eleMail} la mail non è presente nel nostro sistema, riprovare.`;
+    // }
 }
 )
+
 
 
 
